3280312b2164c5e0a368e0079597eff0371f47ae
bautibustos <47929345+bautibustos@users.noreply.github.com>
2024-12-16 10:12:46 -0300

    Version estable
    Version estable.
    Se arreglo el bug del bucle que no finalizaba
    Se agrego archivo de configuracion con tiempo y cantidad de mensajes

--- Modulos/utils.py
+++ Modulos/utils.py
@@ -1,3 +1,6 @@
 import streamlit as st
 def ErrorCarga(error_mensaje):
     st.error(icon="🚨",body=f"ERROR \n{error_mensaje}")
+
+def Warning(warning_mensaje):
+    st.warning(icon="⚠️",body=f"ALERTA \n{warning_mensaje}")
--- SendMsg.py
+++ SendMsg.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 import webbrowser,time, os, pyautogui, random
 import Modulos.CopyImg as CopyImg
 import Modulos.ManejoExel as LE
-import win32gui, win32con, Modulos.utils as utils    
+import win32gui, win32con, Modulos.utils as utils 
+import json   
 #from Interface import ErrorCarga
 """
 Problemas a revisar:
@@ -11,14 +13,20 @@ Pendientes:
     - revisar formato de los numeros
 """
 
+
 #carpeta donde se guarda la imagen
 path = os.getcwd()+"\\Files"
 
+def CargarJson():
+    with open(f"{os.getcwd()}\\config.json", 'r', encoding='utf-8') as archivo:
+        return json.load(archivo)
+    
 def AddImagen(nombre_imagen):
     try:#intenta ver si hay imagen, si la hay no hay error, si no hay error copia la imagen
         CopyImg.Copy(f"{path}\\{nombre_imagen}")
     except Exception as e:
-        print("Error al copiar la imagen: ",e)
+        utils.ErrorCarga(error_mensaje= "Problemas con la imagen")
+        #print("Error al copiar la imagen: ",e)
 
 def Send(mensaje = None,
         imagen = None, imagen_texto = None,
@@ -27,6 +35,9 @@ def Send(mensaje = None,
     flag_nombre = bool
     #ubicacion del exel
     path_file =  f"{path}\\{nombre_archivo}"
+    
+    tiempos = CargarJson()["tiempo"]
+    cant_msg = CargarJson()["cantidad_mensajes"]
 
     #Preguntamos si esta la columna
     if LE.LevantarNumeros(path_file) is False:
@@ -68,92 +79,101 @@ def Send(mensaje = None,
     for numero in numeros_de_telefono:
         
         try:
-            numero= int(numero)#intenta volverlo entero pasa sacar decimales
+            numero = int(numero)#intenta volverlo entero pasa sacar decimales
         except:
             pass
         
-        # si numero no esta vacio
-        if str(numero) != "nan":# convierte el numero en str para encontrar el nan
-            item = str(numero)#lo convierto enstr
-            if not "+54" in item:#me fijo tiene el mas +54
-                #comparo si los primeros dos items son 54
-                if not "54" in item[:2]:
-                    item = "54"+item#agro el 54 si no lo tiene
-                if not "+" in item:# pregunto si tengo el +
-                    item = "+"+item# agrego el + si no esta
-            #elimino guiones de por medio
-            if "-" in item:
-                item.replace('-','')
-            #elimino espacios si hay
-            if " " in item:
-                item.replace(' ','')
-            # limpiar parentesis
-            if "(" in item:
-                item.replace('(', '')
-            # limpiar parentesis
-            if ')' in item:
-                item.replace(')', '')
+        """
+        agregar para que la cantidad de mensajes la defina en el archivo de config
+        """
+        # si supera a los 200 mensajes
+        if contador_nombre == 195:#aviso de alerta qque se llegaron a los 200 msg
+            utils.Warning(warning_mensaje=f" Estas por llegar al limite de 200 mensajes.")
+            break
+        else:
+            if cant_msg == contador_nombre:
+                break
+            else:
+                if str(numero) != "nan":# convierte el numero en str para encontrar el nan
+                    item = str(numero)#lo convierto enstr
+                    if not "+54" in item:#me fijo tiene el mas +54
+                        #comparo si los primeros dos items son 54
+                        if not "54" in item[:2]:
+                            item = "54"+item#agro el 54 si no lo tiene
+                        if not "+" in item:# pregunto si tengo el +
+                            item = "+"+item# agrego el + si no esta
+                    #elimino guiones de por medio
+                    if "-" in item:
+                        item.replace('-','')
+                    #elimino espacios si hay
+                    if " " in item:
+                        item.replace(' ','')
+                    # limpiar parentesis
+                    if "(" in item:
+                        item.replace('(', '')
+                    # limpiar parentesis
+                    if ')' in item:
+                        item.replace(')', '')
 
-            numero = item # reemplazo el valor nuevo
-            
-            #abre Whatsapp desktop dentro del chat del numero y utilizando el texto enviado
-            webbrowser.open(f'whatsapp://send?phone={numero}')#&text={text}
+                    numero = item # reemplazo el valor nuevo
+                    
+                    #abre Whatsapp desktop dentro del chat del numero y utilizando el texto enviado
+                    webbrowser.open(f'whatsapp://send?phone={numero}')#&text={text}
 
-            #espera para darle tiempo a abrir el chat
-            time.sleep(random.choice([2.7, 2.8, 2.9]))
+                    #espera para darle tiempo a abrir el chat
+                    time.sleep(random.choice([2.7, 2.8, 2.9]))
 
-            #Revisar si hay que enviar imagen
-            if imagen != None:
+                    #Revisar si hay que enviar imagen
+                    if imagen != None:
 
-                # pega la imagen
-                pyautogui.hotkey("ctrl","v")
+                        # pega la imagen
+                        pyautogui.hotkey("ctrl","v")
 
-                # tiempo de espera en lo que carga la imagen
-                time.sleep(random.choice([2.7, 2.8, 2.9]))
+                        # tiempo de espera en lo que carga la imagen
+                        time.sleep(random.choice([2.7, 2.8, 2.9]))
 
-                # Si el texto de la imagen no esta vacio lo escribe
-                if imagen_texto != None:
-                    #si es True es xq hay nombre y lo remplaza por el valor de la lista
-                    try:
-                        if flag_nombre:
-                            imagen_edit = imagen_texto.replace("USUARIO", nombre[contador_nombre])
-                            pyautogui.write(imagen_edit)
-                    except:
-                        pyautogui.write(imagen_edit)
-                
-                #esper antes de enviar la imagen
-                time.sleep(random.choice([2.7, 2.8, 2.9]))
+                        # Si el texto de la imagen no esta vacio lo escribe
+                        if imagen_texto != None:
+                            #si es True es xq hay nombre y lo remplaza por el valor de la lista
+                            try:
+                                if flag_nombre:
+                                    imagen_edit = imagen_texto.replace("USUARIO", nombre[contador_nombre])
+                                    pyautogui.write(imagen_edit)
+                            except:
+                                pyautogui.write(imagen_edit)
+                        
+                        #esper antes de enviar la imagen
+                        time.sleep(random.choice([tiempos+0.7, tiempos+0.8, tiempos+0.9]))
 
-                # enviar mensaje
-                pyautogui.press("enter")
+                        # enviar mensaje
+                        pyautogui.press("enter")
 
-                #espera entre proximo chat y / o mensaje
-                time.sleep(random.choice([0.6, 0.7, 0.8]))
-                
+                        #espera entre proximo mensaje
+                        time.sleep(random.choice([0.6, 0.7, 0.8]))
+                        
 
-            #detecta que encuentre un mensaje
-            if mensaje != None:
-            # Escribir mensaje
-                try:
-                    if flag_nombre:#si es que hay usuario, reemplaza por el valor de la lista
-                        mensaje_edit =  mensaje.replace("USUARIO", nombre[contador_nombre])
-                except:
-                    mensaje_edit
-                #escribo el mensaje manualmente
-                pyautogui.write(mensaje_edit)
-                #tiempo de espera antes de enviar el mensaje
-                time.sleep(random.choice([2.3, 2.2, 2,4]) )
-                #enviar mensaje
-                pyautogui.press("enter")
-            
-            #contador para recorrer
-            contador_nombre +=1
-            
-            #espera para abrir proximo chat
-            time.sleep(random.choice([2.5, 2.4, 2.3]))
-
-        else:# suma al contador de nombres para contar la vuelta
-            contador_nombre +=1
+                    #detecta que encuentre un mensaje
+                    if mensaje != None:
+                    # Escribir mensaje
+                        try:
+                            if flag_nombre:#si es que hay usuario, reemplaza por el valor de la lista
+                                mensaje_edit =  mensaje.replace("USUARIO", nombre[contador_nombre])
+                        except:
+                            mensaje_edit
+                        #escribo el mensaje manualmente
+                        pyautogui.write(mensaje_edit)
+                        #tiempo de espera antes de enviar el mensaje
+                        time.sleep(random.choice([0.3+tiempos, tiempos+0.2, tiempos+0.4]) )
+                        #enviar mensaje
+                        pyautogui.press("enter")
+                    
+                    #contador para recorrer
+                    contador_nombre +=1
+                    
+                    #espera para abrir proximo chat
+                    time.sleep(random.choice([tiempos+0.5, tiempos+0.4, tiempos+0.3]))
+                else:
+                    contador_nombre +=1
         
 
 if __name__ == "__main__":
